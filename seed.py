396ac470d534c452a1ea36e635e5020682274ac1
Alyssa <alyssalew2@gmail.com>
2018-07-29 02:13:48 +0000

    Change name of person

--- seed.py
+++ seed.py
@@ -14,7 +14,7 @@ def sample_user():
     password = "123"
     hashed_pw = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-    alyssa = User(first_name="George", last_name="Smith", email="gerge@example.com", password=hashed_pw, created_at='2018-07-28', reliability=10, ranking=10, credit_card_id=1)
+    alyssa = User(first_name="Madeline", last_name="Sanchez", email="madeline@example.com", password=hashed_pw, created_at='2018-07-28', reliability=10, ranking=10, credit_card_id=1)
     db.session.add(alyssa)
     db.session.commit()
 
